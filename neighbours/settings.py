cf1ed29e394b8d93fb8a88edfa2f58028e3d7260
Mark-Muiri <mark.muiri@student.moringaschool.com>
2021-09-24 11:21:32 +0300

    created db and made first migrations

--- neighbours/settings.py
+++ neighbours/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'neighbours.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'watch',
+        'USER': 'waichinga',
+    'PASSWORD':'waichinga',
     }
 }
 
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
