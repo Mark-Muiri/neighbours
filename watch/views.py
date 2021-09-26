90550ef966c6386434d25ef701a0486211143ffc
Mark-Muiri <mark.muiri@student.moringaschool.com>
2021-09-26 14:49:37 +0300

    imports

--- watch/views.py
+++ watch/views.py
@@ -1,4 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Neighbourhood,Business,User
+
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+
+
+
+from django.contrib.auth.models import User
+import statistics
 
 # Create your views here.
 
