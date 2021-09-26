0e57927412152e9f55f0d49644136ffff01a562e
Mark-Muiri <mark.muiri@student.moringaschool.com>
2021-09-26 14:46:48 +0300

    created a view function to display the indec page

--- watch/views.py
+++ watch/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """
+    Displays landing page
+    """
+    title = "T.N.W"
+    business = business.display_all_business()
+   
+
+    return render(
+        request,
+        "index.html",
+        {"title": title, "business": business,},
+    )
