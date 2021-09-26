f4274fa0feb5e5c6b5eefcccad47276f2a955f5b
Mark-Muiri <mark.muiri@student.moringaschool.com>
2021-09-26 12:15:42 +0300

    added bootstrap

--- neighbours/settings.py
+++ neighbours/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'watch',
     'cloudinary',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
